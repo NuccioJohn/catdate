478aa63699eb892300a203cf1d4c0580b79b05d4
John Lester Nuccio <nuccio.john@gmail.com>
2014-05-22 14:32:06 -0700

    Saving just in case, can't get profiles to store user_id's properly

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140522050327) do
+ActiveRecord::Schema.define(version: 20140522205357) do
 
   create_table "profiles", force: true do |t|
     t.string   "about"
     t.string   "breed"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
